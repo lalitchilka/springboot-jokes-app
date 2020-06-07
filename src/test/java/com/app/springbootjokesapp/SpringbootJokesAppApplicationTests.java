29577c82037a561abf13cb347ab3ae29a9f01a76
lalitchilka <lalitchilka@gmail.com>
2020-06-07 22:45:51 +0200

    Initial Project commit

---
+++ src/test/java/com/app/springbootjokesapp/SpringbootJokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.springbootjokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJokesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
